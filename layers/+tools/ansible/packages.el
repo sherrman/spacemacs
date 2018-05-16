01e847ece59e4625026906906b8678303d233464
Codruț Constantin Gușoi <codrut.gusoi@gmail.com>
2018-05-16 19:44:47 +0100

    Fixes ansible key errors
    https://github.com/syl20bnr/spacemacs/commit/a3debe157032fc0999044eeb060d1e83107c87ee

--- layers/+tools/ansible/packages.el
+++ layers/+tools/ansible/packages.el
@@ -22,26 +22,32 @@
     :init
     (progn
       (add-hook 'yaml-mode-hook 'spacemacs/ansible-maybe-enable)
-      (if ansible-auto-encrypt-descrypt
-          (add-hook 'ansible-hook 'ansible::auto-decrypt-encrypt)
-        (remove-hook 'ansible-hook 'ansible::auto-decrypt-encrypt))
-      (spacemacs/set-leader-keys-for-minor-mode 'ansible
-        "bd" 'ansible::decrypt-buffer
-        "be" 'ansible::encrypt-buffer))
+      (put 'ansible::vault-password-file 'safe-local-variable #'stringp)
+      (if ansible-auto-encrypt-decrypt
+          ;; add this hook to local-vars-hook to allow users to specify
+          ;; a password file in directory local variables
+          (add-hook 'yaml-mode-local-vars-hook
+                    'spacemacs/ansible-auto-decrypt-encrypt-vault)
+        (remove-hook 'yaml-mode-local-vars-hook
+                     'spacemacs/ansible-auto-decrypt-encrypt-vault))
+      )
     :config
-    ;; TODO to remove when fixed upstream
-    (advice-add 'ansible::decrypt-buffer
-                :after 'spacemacs//ansible-reset-buffer-modified)))
+    (progn
+      (spacemacs/set-leader-keys-for-minor-mode 'ansible
+       "bd" 'ansible::decrypt-buffer
+       "be" 'ansible::encrypt-buffer))))
 
 (defun ansible/init-ansible-doc ()
   (use-package ansible-doc
     :defer t
     :init
     (progn
-      (add-hook 'yaml-mode-hook 'spacemacs/ansible-doc-maybe-enable)
+      (add-hook 'yaml-mode-hook 'spacemacs/ansible-doc-maybe-enable))
+    :config
+    (progn
       (spacemacs/set-leader-keys-for-minor-mode 'ansible-doc-mode
-        "ha" 'ansible-doc))
-    :config (spacemacs|hide-lighter ansible-doc-mode)))
+        "ha" 'ansible-doc)
+      (spacemacs|hide-lighter ansible-doc-mode))))
 
 (defun ansible/post-init-company ()
   ;; ansible-mode requires ac-user-dictionary-files. If the
